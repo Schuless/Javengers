7b599d7fb8ac4609b230c57f98bf0d22b80fb0a9
Henrique Schulenburg <henrique.schulis@gmail.com>
2024-11-25 15:13:38 -0300

    feat(creating user html files, and updating all the lists from all the folders)

--- src/main/java/com/senai/javengers/controller/UsuarioController.java
+++ src/main/java/com/senai/javengers/controller/UsuarioController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/usuario")
+@RequestMapping("/usuarios")
 @Controller
 public class UsuarioController {
 
