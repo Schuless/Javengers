b9b30864bfb7a4bbe7a0f841b98d8f65900ce401
Henrique Schulenburg <henrique.schulis@gmail.com>
2024-12-01 23:16:44 -0300

    fix(logout)

--- src/main/java/com/senai/javengers/controller/LoginViewController.java
+++ src/main/java/com/senai/javengers/controller/LoginViewController.java
@@ -13,37 +13,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping()
 @Controller
 public class LoginViewController {
-    
+
     @Autowired
     private UsuarioService usuarioService;
-    
+
     @GetMapping("/login")
-    public String exibirLogin(Model model){
-        
+    public String exibirLogin(Model model) {
+
         LoginDto loginDto = new LoginDto();
-        
+
         model.addAttribute("loginDto", loginDto);
-        
+
         //--template : retorna o nome do arquivo html localizado lá na pasta templates.
         return "login";
     }
-    
+
     @PostMapping("/login")
-    public String realizarLogin(@ModelAttribute("loginDto") LoginDto loginDto){           
-        
+    public String realizarLogin(@ModelAttribute("loginDto") LoginDto loginDto) {
+
         //--Chamar método da classe UsuarioService passando por parâmetro o Dto
         boolean acesso = usuarioService.validarLogin(loginDto);
-        
-        if(acesso){            
+
+        if (acesso) {
             return "redirect:home/index";
         }
-        
+
         return "redirect:login?erro";
-        
-    } 
-        
+
+    }
+
     @PostMapping("/logout")
-    public String realizarLogout(){        
-        return "redirect:login?logout";
+    public String realizarLogout() {
+        usuarioService.deslogarUsuario();
+        return "redirect:login";
     }
 }
